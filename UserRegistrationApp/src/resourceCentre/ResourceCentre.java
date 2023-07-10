adc430731b17b75892b5ff7275651c31f7429e8e
22028442 <22028442@10.175.8.26>
2023-07-10 15:58:32 +0800

    ResourceCenter

--- UserRegistrationApp/src/resourceCentre/ResourceCentre.java
+++ UserRegistrationApp/src/resourceCentre/ResourceCentre.java
@@ -2,4 +2,20 @@ package resourceCentre;
 
 public class ResourceCentre {
 
+	public void main(String [] args)
+	{
+		
+	}
+	public void menu()
+	{
+		
+	}
+	public void setHeader(String a)
+	{
+		
+	}
+	public String showAvailability(boolean a)
+	{
+		
+	}
 }
